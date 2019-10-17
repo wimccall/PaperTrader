2fdccb3f72ed474cc16d1308824d3275a1b26cf7
WadeMcCall <wimccall42@students.tntech.edu>
2019-10-17 12:27:22 -0500

    Improve file structure

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -24,7 +24,7 @@ function createWindow () {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html');
+  mainWindow.loadFile('assets/html/index.html');
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
--- assets/JS/renderer.js
+++ assets/JS/renderer.js
@@ -3,17 +3,18 @@
 // All of the Node.js APIs are available in this process.
 
 // Watchlist class. Takes care of the list itself and the accompanying UI
-var watchlistTable = require('./watchlist');
+console.log(__dirname);
+var watchlistTable = require("../JS/watchlist");
 var watchlistTableClass = watchlistTable.watchlist;
 var watchlist = new watchlistTableClass();
 
 // Trading api class
-IEXAPI = require('./IEXAPI');
+IEXAPI = require('../JS/IEXAPI');
 apiClass = IEXAPI.api;
 api = new apiClass();
 
 // Search class
-search = require('./searchstock');
+search = require('../JS/searchstock');
 searchClass = search.search;
 search = new searchClass();
 
